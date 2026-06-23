5964d9eb17184e823abeebbab9b72a10f9eaa2e3
Miguelgsd <miguelcbdutra@gmail.com>
2026-06-23 19:42:29 -0300

    Implementação dos métodos para registrar transações e buscar o histórico

--- banco/dao/ContaPoupancaDAO.java
+++ banco/dao/ContaPoupancaDAO.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
 
 public class ContaPoupancaDAO{
     
@@ -31,7 +33,7 @@ public class ContaPoupancaDAO{
         return null;
     }
     
-    public boolean atualizarSaldo(String numeroConta, double novoSaldo){
+    public boolean atualizar(String numeroConta, double novoSaldo){
         String sql = "UPDATE contas_poupanca SET saldo = ? WHERE numeroConta = ?";
         
         try (Connection conn = ConexaoDB.getConnection();
@@ -48,5 +50,51 @@ public class ContaPoupancaDAO{
             System.err.print("Erro ao atualizar os dados: " + e);
             return false;
         }
+    }
+    
+    public boolean registrarTransacao(int contaId, String descricao, double valor){
+        String sql = "INSERT INTO transacoes (conta_id, tipo_conta, descricao, valor) VALUES (?, POUPANCA, ?, ?)";
+        
+        try (Connection conn = ConexaoDB.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)){
+            
+            stmt.setInt(1, contaId);
+            stmt.setString(2, descricao);
+            stmt.setDouble(3, valor);
+            
+            int linhasAfetadas = stmt.executeUpdate();
+            
+            return linhasAfetadas > 0;
+            
+        } catch (SQLException e){
+            System.err.print("Não foi possível registrar a transação: " + e);
+            return false;
+        }
+    }
+    
+    public List buscarHistorico(int contaId){
+        List<String> historico = new ArrayList<>();
+        String sql = "SELECT descricao, valor, data_hora FROM transacoes WHERE contaId = ?";
+        
+        try(Connection conn = ConexaoDB.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+            
+            stmt.setInt(1, contaId);
+            
+            try(ResultSet rs = stmt.executeQuery()){
+                while(rs.next()){
+                    String descricao = rs.getString("descricao");
+                    double valor = rs.getDouble("valor");
+                    String data = rs.getTimestamp("data_hora").toString();
+                    
+                    String linha = data + " - " + descricao + ": R$ " + String.format("%.2f", valor);
+                    historico.add(linha);
+                }
+            }
+        
+        } catch(SQLException e){
+            System.err.print("Não foi possível buscar o histórico: " + e);
+        }
+        return historico;
     }
 }
