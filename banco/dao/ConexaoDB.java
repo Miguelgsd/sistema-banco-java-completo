a5776ca49d079cfb3466359d9af701275f0eeade
Miguelgsd <miguelcbdutra@gmail.com>
2026-06-30 17:21:19 -0300

    Criação do db.properties

--- banco/dao/ConexaoDB.java
+++ banco/dao/ConexaoDB.java
@@ -1,23 +1,52 @@
 package banco.dao;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConexaoDB {
+    
+    private static Properties carregarPropriedades() {
+        Properties props = new Properties();
 
-    private static final String URL = System.getProperty("DB_URL");
-    private static final String USUARIO = System.getProperty("DB_USER");
-    private static final String SENHA = System.getProperty("DB_PASSWORD");
+        try (InputStream is = ConexaoDB.class.getClassLoader().getResourceAsStream("db.properties")) {
+            if (is == null) {
+                System.err.println("Aviso: Arquivo 'db.properties' não encontrado nos recursos. Usando contingência.");
+                return null;
+            }
+            props.load(is);
+        } catch (IOException e) {
+            System.err.println("Erro ao carregar o arquivo db.properties: " + e.getMessage());
+        }
+        return props;
+    }
 
     public static Connection getConnection() throws SQLException {
+        Properties props = carregarPropriedades();
+        
+        String url;
+        String user;
+        String password;
+
+        if (props != null && !props.isEmpty()) {
+            url = props.getProperty("db.url");
+            user = props.getProperty("db.user");
+            password = props.getProperty("db.password");
+        } else {
+            // Linha de contingência caso queira testar com valores padrão locais temporariamente
+            url = "";
+            user = "";
+            password = "";
+        }
+
         try {
             Class.forName("org.postgresql.Driver");
-            
-            return DriverManager.getConnection(URL, USUARIO, SENHA);
-            
+            return DriverManager.getConnection(url, user, password);
         } catch (ClassNotFoundException e) {
-            throw new SQLException("Erro: Driver JDBC do PostgreSQL não foi encontrado nas bibliotecas.", e);
+            throw new SQLException("Driver do PostgreSQL não foi localizado no projeto: " + e.getMessage());
         }
     }
 }
